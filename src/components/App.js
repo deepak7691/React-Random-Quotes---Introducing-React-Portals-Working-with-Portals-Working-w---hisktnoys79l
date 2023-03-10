cd23764cc5b2b8d326dfefb9c88d36430c8c46c0
deepak7691 <kawliya11@gmail.com>
2023-03-10 17:27:42 +0530

    deepak

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,39 @@
-import React from "react";
+// import React from "react";
+// import "../styles/App.css";
+
+// var colors = [
+//   '#16a085',
+//   '#27ae60',
+//   '#2c3e50',
+//   '#f39c12',
+//   '#e74c3c',
+//   '#9b59b6',
+//   '#FB6964',
+//   '#342224',
+//   '#472E32',
+//   '#BDBB99',
+//   '#77B1A9',
+//   '#73A857'
+// ];
+
+// const App = () => {
+
+//     return (
+//       <div id="main">
+//         <div id="wrapper">
+          
+//         </div>
+//       </div>
+//     );
+// };
+
+// export default App;
+
+
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
 
-var colors = [
+const colors = [
   '#16a085',
   '#27ae60',
   '#2c3e50',
@@ -17,14 +49,48 @@ var colors = [
 ];
 
 const App = () => {
+  const [quote, setQuote] = useState({});
 
-    return (
-      <div id="main">
-        <div id="wrapper">
-          
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then(response => response.json())
+      .then(data => {
+        setQuote(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const handleNewQuote = () => {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+    fetchQuote();
+  };
+
+  return (
+    <div id="main">
+      <div id="wrapper">
+        <div id="quote-box">
+          <div id="text" className="quote-text">
+            {quote.content}
+          </div>
+          <div id="author" className="quote-author">
+            {quote.author}
+          </div>
+          <div className="buttons">
+            <button id="new-quote" onClick={handleNewQuote}>
+              New Quote
+            </button>
+          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default App;
